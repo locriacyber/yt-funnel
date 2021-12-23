0e9b5c43ee0e042d4d5daeb591eb440adeb4cd4e
Locria Cyber <locriacyber@noreply.users.github.com>
2021-12-23 21:42:28 +0800

    Made basic UI

--- hello.py
+++ hello.py
@@ -1,12 +1,42 @@
 import dearpygui.dearpygui as dpg
-from dearpygui.demo import show_demo
 
-dpg.create_context()
-dpg.create_viewport()
-dpg.setup_dearpygui()
+rem = 12
 
-show_demo()
+def on_main_window_close():
+    exit(0)
 
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
+def save_callback():
+    print("Save Clicked")
+
+def main():
+    tasks = []
+    def add_task():
+        nonlocal tasks
+        pass
+
+
+    dpg.create_context()
+
+    with dpg.window(label="Main Window", autosize=True, on_close=on_main_window_close):
+        with dpg.collapsing_header(label="Add Task", leaf=True):
+            with dpg.group(indent=rem):
+                with dpg.group(horizontal=True):
+                    dpg.add_input_text(hint="URL")
+                    dpg.add_button(label="Add Task", callback=add_task)
+                dpg.add_checkbox(label="Add on Clipboard Change")
+        with dpg.collapsing_header(label="Staging", leaf=True):
+            with dpg.group(indent=rem):
+                dpg.add_text("Pending Tasks")
+                listbox_tasks = dpg.add_listbox()
+        with dpg.collapsing_header(label="Downloading", leaf=True):
+            with dpg.group(indent=rem):
+                dpg.add_text("TODO")
+
+    dpg.create_viewport()
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
+
+if __name__ == "__main__":
+    main()
